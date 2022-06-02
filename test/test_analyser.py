f1ee22fc6595ee9bd90eae566fd020d50cef9c77
Fei Chen <boxchenfei@gmail.com>
2022-06-02 15:18:38 +0200

    Add unit tests for Function

--- test/test_analyser.py
+++ test/test_analyser.py
@@ -1,6 +1,7 @@
+import inspect
 import pytest
 from test_data import package_example
-from VPLCodeGenerator.analyser import Variable, _docstr, _cut
+from VPLCodeGenerator.analyser import Variable, Function, _docstr, _cut, _PrettySignature
 from VPLCodeGenerator.inspect_util import empty
 
 
@@ -57,3 +58,137 @@ class TestVariables:
                 raise Exception
         self.v.default_value = C()
         assert self.v.default_value_str == " = <unable to get value representation>"
+
+
+def new_function_in(name, in_class='ClassB', taken_from_class='ClassB'):
+    modulename = 'package_example'
+    qualname = f'{in_class}.{name}'
+    obj = getattr(package_example, taken_from_class).__dict__[name]
+    return Function(modulename, qualname, obj, (modulename, f'{taken_from_class}.{name}'))
+
+
+class TestFunction:
+
+    @pytest.mark.parametrize("name, func, taken_from", [('staticmethod', package_example.ClassB.__dict__['staticmethod']
+                                                         , 'ClassB'),
+                                                        ('classmethod', package_example.ClassB.__dict__['classmethod']
+                                                         , 'ClassB'),
+                                                        ('update_attr3', package_example.ClassA.__dict__['update_attr3']
+                                                         , 'ClassA')])
+    def test_function_attributes(self, name, func, taken_from):
+        f = new_function_in(name, taken_from, taken_from)
+        assert f.wrapped == func
+        if isinstance(func, (classmethod, staticmethod)):
+            func = func.__func__
+        assert f.obj == func
+        assert f.parser_config is None
+        assert f.taken_from == ('package_example', f'{taken_from}.{name}')
+
+    def test_full_name(self):
+        name = 'staticmethod'
+        f = new_function_in(name)
+        assert f.fullname == f'{f.modulename}.{f.qualname}'
+
+    def test_name(self):
+        name = 'staticmethod'
+        f = new_function_in(name)
+        assert f.name == name
+
+    @pytest.mark.parametrize("name, taken_from, is_classmethod", [('staticmethod', 'ClassB', False),
+                                                                  ('classmethod', 'ClassB', True),
+                                                                  ('update_attr3', 'ClassA', False)])
+    def test_is_classmethod(self, name, taken_from, is_classmethod):
+        f = new_function_in(name, taken_from, taken_from)
+        assert f.is_classmethod == is_classmethod
+
+    @pytest.mark.parametrize("name, taken_from, is_staticmethod", [('staticmethod', 'ClassB', True),
+                                                                   ('classmethod', 'ClassB', False),
+                                                                   ('update_attr3', 'ClassA', False)])
+    def test_is_staticmethod(self, name, taken_from, is_staticmethod):
+        f = new_function_in(name, taken_from, taken_from)
+        assert f.is_staticmethod == is_staticmethod
+
+    def test_docstring(self):
+        name = 'get_attr6'
+        f = new_function_in(name)
+        assert f.docstring == 'function docstring, Overrides version from class A'
+
+    def test_docstring_of_inherited_method(self):
+        name = 'classmethodinA'
+        f = new_function_in(name, taken_from_class='ClassA')
+        assert f.docstring == 'class method docstring in Class A'
+
+    def test_docstring_of_init(self):
+        name = '__init__'
+        f = new_function_in(name, 'ClassA', 'ClassA')
+        assert f.docstring == ''
+
+    def test_decorators(self):
+        name = 'classmethodinA'
+        f = new_function_in(name, taken_from_class='ClassA')
+        assert f.decorators == ['@classmethod']
+
+    @pytest.mark.parametrize("name, funcdef", [('get_attr6', 'def'), ('async_fun', 'async def')])
+    def test_funcdef(self, name, funcdef):
+        f = new_function_in(name)
+        assert f.funcdef == funcdef
+
+    def test_parameters(self):
+        name = 'classmethod'
+        obj = package_example.ClassB.__dict__[name]
+        actual = list(inspect.signature(obj.__func__).parameters.values())[1:]
+        f = new_function_in(name)
+        assert f.parameters == actual
+
+    def test_signature(self):
+        name = 'classmethod'
+        obj = package_example.ClassB.__dict__[name]
+        expected = _PrettySignature.from_callable(obj.__func__)
+        f = new_function_in(name)
+        assert f.signature == expected
+
+    def test_signature_object_init(self):
+        f = Function('', '', object.__init__, None)
+        assert f.signature == inspect.Signature()
+
+    def test_signature_without_self(self):
+        # like (a, b, c) -> bool
+        name = 'classmethod'
+        f = new_function_in(name)
+        expected = f.signature.replace(parameters=list(f.signature.parameters.values())[1:])
+        assert f.signature_without_self == expected
+
+    @pytest.mark.parametrize("name, annotations, from_class", [('classmethod', __builtins__['bool'], 'ClassB'),
+                                                               ('init_attr7', empty, 'ClassB'),
+                                                               ('__init__', empty, 'ClassA')])
+    def test_annotations(self, name, annotations, from_class):
+        f = new_function_in(name, from_class, from_class)
+        assert f.annotations == annotations
+
+    def test_repr_classmethod(self):
+        name = 'classmethod'
+        f = new_function_in(name)
+        assert repr(f) == '<@classmethod class def classmethod(\n    cls,\n    arg1: str,\n    arg2: int,\n    /,' \
+                          '\n    arg3: int = 4,\n    *arg4,\n    arg5: int = 4,\n    arg6: float = 5\n) -> bool:' \
+                          '  # class method docstri…>'
+
+    def test_repr_staticmethod(self):
+        name = 'staticmethod'
+        f = new_function_in(name)
+        assert repr(f) == '<@staticmethod static def staticmethod()>'
+
+    def test_repr_method(self):
+        name = 'get_attr6'
+        modulename = 'package_example'
+        qualname = f'ClassB.{name}'
+        obj = getattr(package_example, 'ClassB').__dict__[name]
+        f = Function(modulename, qualname, obj, (modulename, f'ClassA.{name}'))
+        assert repr(f) == '<method def get_attr6(self, arg1, arg2, /):  ' \
+                          '# inherited from package_example.ClassA.get_attr6, function docstring, …>'
+
+    def test_repr_function(self):
+        name = 'module_level_function'
+        modulename = 'package_example'
+        f = Function(modulename, name, package_example.module_level_function, (modulename, name))
+        assert repr(f) == "<function def module_level_function(arg1, arg2='default', *, arg3, **kwargs) -> float:" \
+                          "  # function docstring>"
