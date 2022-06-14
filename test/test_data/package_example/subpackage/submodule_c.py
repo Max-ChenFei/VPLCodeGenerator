55449f676d199d7f7143984a34bd78cfe4f2a87d
Fei Chen <boxchenfei@gmail.com>
2022-06-14 20:54:30 +0200

    Add inheritance test data

--- test/test_data/package_example/subpackage/submodule_c.py
+++ test/test_data/package_example/subpackage/submodule_c.py
@@ -1,6 +1,9 @@
-class SubmoduleC:
+from ..submodule_b import SubmoduleB
+
+
+class SubmoduleC(SubmoduleB):
     def __init__(self, name):
         self.name = name
-        
+
     def myname(self):
         return self.name
