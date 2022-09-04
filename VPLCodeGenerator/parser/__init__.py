d07cfb9fe39c9cb496fb14613697dac2e2d6deb7
Fei Chen <boxchenfei@gmail.com>
2022-09-04 23:51:39 +0200

    Add ParserSuitsRegistry, ParserSuit and cleanup parser base class

--- VPLCodeGenerator/parser/__init__.py
+++ VPLCodeGenerator/parser/__init__.py
@@ -1,2 +1,5 @@
 from .source_code_parser import VariableParser
-from .source_code_parser import SourceCodeParser, SourceCodeModuleParser, SourceCodeClassParser, obj_type
+from .source_code_parser import SourceCodeParser, SourceCodeModuleParser,\
+    SourceCodeClassParser, SourceCodeParserSuit, resolve_annotations
+from .reST_parser import ReSTParserSuit, ReSTParser
+from .parser import ParserSuitsRegistry
--- VPLCodeGenerator/parser/parser.py
+++ VPLCodeGenerator/parser/parser.py
@@ -1,30 +1,42 @@
 from abc import ABC, abstractmethod
-from functools import cached_property
+
+
+class ParserSuitsRegistry:
+    def __init__(self):
+        self.suits = {}
+        for cls in ParserSuit.__subclasses__():
+            self.register_parser_suit_type(cls.name(), cls)
+
+    def register_parser_suit_type(self, name, obj):
+        self.suits.setdefault(name, obj)
+
+    def __getitem__(self, name):
+        if name in self.suits.keys():
+            return self.suits[name]
+        raise Exception(f'{name} do not support.')
+
+    def available_parser_suit_types(self):
+        return list(self.suits.keys())
+
+
+class ParserSuit(ABC):
+    parser_types = {'default': None}
+
+    @staticmethod
+    def name():
+        NotImplementedError
+
+    def __getitem__(self, item):
+        try:
+            return self.parser_types[item]
+        except:
+            return self.parser_types['default']
 
 
 class Parser(ABC):
-
     @abstractmethod
-    def var_docstring(self):
-        NotImplementedError
-
-    @abstractmethod
-    def var_annotations(self):
-        NotImplementedError
-
-    @abstractmethod
-    def member_objects(self):
+    def members(self):
         NotImplementedError
 
     def submodules(self):
-        NotImplementedError
-
-    @cached_property
-    def vars_sets(self) -> set[str]:
-        return self.var_docstring.keys() | self.var_annotations.keys()
-
-    def definitions(self):
-        """
-            The mapping between object name to its modulename and qualname
-        """
-        NotImplementedError
+        return []
